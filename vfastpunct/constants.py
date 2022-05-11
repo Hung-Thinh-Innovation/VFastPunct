e51d9da16024ffbc2489713ce084ea532a7a22e6
datngo <dat821168@gmail.com>
2022-05-11 15:42:57 +0700

    VFastPunct object

--- vfastpunct/constants.py
+++ vfastpunct/constants.py
@@ -1,7 +1,11 @@
-from log import init_logger
-
+from vfastpunct.log import init_logger
+from vfastpunct.models import PuncBertLstmCrf
 
 LOGGER = init_logger()
 EOS_MARKS = ["PERIOD", "QMARK", "EXCLAM"]
 PUNC_LABEL2ID = ['O', 'PERIOD', 'COMMA', 'COLON', 'QMARK', 'EXCLAM', 'SEMICOLON']
 PUNC_ID2LABEL = {idx: label for idx, label in enumerate(PUNC_LABEL2ID)}
+
+MODEL_MAPPING = {
+    'BertLstmCrf-cased': (PuncBertLstmCrf,)
+}
