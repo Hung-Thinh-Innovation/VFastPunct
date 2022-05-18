70b96f6af70ec0ab56042498e40deb5bd8e7ec84
datngo <dat821168@gmail.com>
2022-05-18 07:17:55 +0700

    make dataset function

--- vfastpunct/processor.py
+++ vfastpunct/processor.py
@@ -1,3 +1,4 @@
+import random
 from typing import List, Union
 from pathlib import Path
 from string import punctuation
@@ -38,13 +39,6 @@ def get_total_lines(fpath: Union[str, os.PathLike]):
     return int(subprocess.check_output(["wc", "-l", fpath]).split()[0])
 
 
-def train_test_split(fpath: Union[str, os.PathLike], total_lines: int, test_ratio=0.2):
-    out_path = str(Path(fpath).parent) + '/raw_'
-    cmd = f'shuf {fpath} | split -a1 -d  -l $({int(total_lines * (1 - test_ratio))}) - {out_path}'
-    subprocess.run(cmd)
-    return Path(out_path+'0'), Path(out_path+'1')
-
-
 def binary_search(a, x):
     i = bisect.bisect_left(a, x)
     if i != len(a) and a[i] == x:
@@ -75,54 +69,48 @@ def make_dataset(data_file: Union[str, os.PathLike], split_test=False, test_rati
     raw_path = Path(data_file)
     debug_count = 0
     total_lines = get_total_lines(raw_path)
-    dataset_mapping = {'train.txt': (raw_path, total_lines), 'test.txt': None}
-
-    if split_test:
-        raw_train_path, raw_test_path = train_test_split(raw_path, total_lines=total_lines, test_ratio=test_ratio)
-        num_train = int(total_lines * (1 - test_ratio))
-        dataset_mapping = {
-            'train.txt': (raw_train_path, num_train),
-            'test.txt': (raw_test_path, total_lines - num_train)
-        }
-    for dtype, (dpath, num_line) in dataset_mapping.items():
-        if dpath is None:
-            continue
-        dwriter = open(Path(str(raw_path.parent) + f'/{dtype}'), 'w')
-        with open(dpath, 'r', encoding='utf-8') as f:
-            for idx, line in enumerate(tqdm(f, total=num_line)):
-                cur_examples = []
-                line = normalize_text(line)
-                matches = punct_pattern.finditer(line)
-                end_idx = 0
-                for m in matches:
-                    tokens = line[end_idx: m.start()].split()
-                    if len(tokens) == 0:
-                        end_idx = m.end()
-                        continue
-                    for t in tokens[:-1]:
-                        cur_examples.extend([(t.lower(), 'O', get_cap_label(t))])
-                    puncs = line[m.start(): m.end()]
-                    punc_label = 'O'
-                    if puncs in STRPUNC_MAPPING:
-                        punc_label = STRPUNC_MAPPING[puncs]
-                    else:
-                        for punc in list(puncs):
-                            if punc in STRPUNC_MAPPING:
-                                punc_label = STRPUNC_MAPPING[punc]
-                                break
-                    cur_examples.append((tokens[-1].lower(), punc_label, get_cap_label(tokens[-1])))
+    train_writer = open(Path(str(raw_path.parent) + '/train.txt'), 'w')
+    test_writer = open(Path(str(raw_path.parent) + '/test.txt'), 'w') if split_test else None
+    with open(raw_path, 'r', encoding='utf-8') as f:
+        for idx, line in enumerate(tqdm(f, total=total_lines)):
+            cur_examples = []
+            line = normalize_text(line)
+            matches = punct_pattern.finditer(line)
+            end_idx = 0
+            for m in matches:
+                tokens = line[end_idx: m.start()].split()
+                if len(tokens) == 0:
                     end_idx = m.end()
-                if not restoration_punct(cur_examples) == line and debug:
-                    debug_count += 1
-                    print(f"===== Error Case {debug_count} =====")
-                    print(line)
-                    print(restoration_punct(cur_examples))
+                    continue
+                for t in tokens[:-1]:
+                    cur_examples.extend([(t.lower(), 'O', get_cap_label(t))])
+                puncs = line[m.start(): m.end()]
+                punc_label = 'O'
+                if puncs in STRPUNC_MAPPING:
+                    punc_label = STRPUNC_MAPPING[puncs]
+                else:
+                    for punc in list(puncs):
+                        if punc in STRPUNC_MAPPING:
+                            punc_label = STRPUNC_MAPPING[punc]
+                            break
+                cur_examples.append((tokens[-1].lower(), punc_label, get_cap_label(tokens[-1])))
+                end_idx = m.end()
+            if not restoration_punct(cur_examples) == line and debug:
+                debug_count += 1
+                print(f"===== Error Case {debug_count} =====")
+                print(line)
+                print(restoration_punct(cur_examples))
+            if random.uniform(0, 1) > test_ratio or not split_test:
                 for example in cur_examples:
-                    dwriter.write(' '.join(example)+'\n')
-        dwriter.close()
+                    train_writer.write(' '.join(example)+'\n')
+            else:
+                for example in cur_examples:
+                    test_writer.write(' '.join(example) + '\n')
+    train_writer.close()
+    test_writer.close()
 
 
 # DEBUG
 if __name__ == "__main__":
-    raw_data_path = './datasets/Raw/corpus-full.txt'
+    raw_data_path = '/media/datngo/Data4/puncdataset/corpus-full.txt'
     make_dataset(raw_data_path, split_test=True, debug=False)
